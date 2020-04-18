b19a3a9d52279b92a1a8bb3ed360ef209e0e60eb
Aurélien Jacomy <jacomya@gmail.com>
2020-04-18 15:01:22 -0300

    Taks Manager done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -14,12 +14,29 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to task_path (@task)
+    redirect_to task_path(@task)
+  end
+
+  def edit; end
+
+  def update
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # 1. Show All Restaurants
   get 'tasks', to: 'tasks#index'
+  # 2. Show One Restaurante
   get 'tasks/new', to: 'tasks#new', as: 'new'
+  # New task
   get 'tasks/:id', to: 'tasks#show', as: 'task'
   post 'tasks', to: 'tasks#create'
-  # 2. Show One Restaurante
+  # Edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'task/:id', to: 'tasks#destroy', as: 'destroy'
 end
