18003a13758ccbb7521a01f5102eb9d25b7ba058
Aurélien Jacomy <jacomya@gmail.com>
2020-04-18 12:44:50 -0300

    New task page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # 1. Show All Restaurants
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: 'new'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  post 'tasks', to: 'tasks#create'
   # 2. Show One Restaurante
 end
